b8a42c2cc569d616bb35d7b2286d49690be67f59
mehdibidjed <mehdibidjed25@gmail.com>
2026-02-03 01:33:13 +0100

    code structure

---
+++ src/pages/industries/HealthCare.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HealthCare() {
+
+    // this is the code part of ibaa 
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HealthCare
---
+++ src/pages/services/MachineLearning.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MachineLearning() {
+    // Ahmed part 
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MachineLearning
