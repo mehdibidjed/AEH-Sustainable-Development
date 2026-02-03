a9f3b49a2f66af54b342fc0e7c7ff23b3aef379a
mehdibidjed <mehdibidjed25@gmail.com>
2026-02-03 10:33:25 +0100

    add ML service assets

--- src/assets/Asset.js
+++ src/assets/Asset.js
@@ -1,55 +1,81 @@
 import AppIcon from "./icons/app_icon.svg";
 import BG1 from "./images/home/background_1.svg";
 import Tech from "./images/home/techno_card.svg";
-import ML from './images/home/machine_learning.svg'
-import BuyNow from "./images/home/buy_now.svg"
-import icon1 from './icons/home/icon1.svg'
-import icon2 from './icons/home/icon2.svg'
-import icon3 from './icons/home/icon3.svg'
-import icon4 from './icons/home/icon4.svg'
-import icon5 from './icons/home/icon5.svg'
-import icon6 from './icons/home/icon6.svg'
-import icon7 from './icons/home/icon7.svg'
-import icon8 from './icons/home/icon8.svg'
-import icon9 from './icons/home/icon9.svg'
-import icon10 from './icons/home/icon10.svg'
-import icon11 from './icons/home/icon11.svg'
-import icon12 from './icons/home/icon12.svg'
-import icon13 from './icons/home/icon13.svg'
-import icon14 from './icons/home/icon14.svg'
-import CreativeDes from  './images/home/creative_design.svg'
-import CreativeChat from './images/home/creative_chat.svg'
-import Agriculture from './images/home/agriculture.svg'
+import ML from "./images/home/machine_learning.svg";
+import BuyNow from "./images/home/buy_now.svg";
+import icon1 from "./icons/home/icon1.svg";
+import icon2 from "./icons/home/icon2.svg";
+import icon3 from "./icons/home/icon3.svg";
+import icon4 from "./icons/home/icon4.svg";
+import icon5 from "./icons/home/icon5.svg";
+import icon6 from "./icons/home/icon6.svg";
+import icon7 from "./icons/home/icon7.svg";
+import icon8 from "./icons/home/icon8.svg";
+import icon9 from "./icons/home/icon9.svg";
+import icon10 from "./icons/home/icon10.svg";
+import icon11 from "./icons/home/icon11.svg";
+import icon12 from "./icons/home/icon12.svg";
+import icon13 from "./icons/home/icon13.svg";
+import icon14 from "./icons/home/icon14.svg";
+import CreativeDes from "./images/home/creative_design.svg";
+import CreativeChat from "./images/home/creative_chat.svg";
+import Agriculture from "./images/home/agriculture.svg";
+import AiImage from "./images/services/ml/ai.svg";
+import WomenImage from "./images/services/ml/woman.svg";
+import HaggingFaceIcon from "./icons/services/ml/huggingface.svg";
+import Apache from "./icons/services/ml/apache.svg";
+import AWS from "./icons/services/ml/aws.svg";
+import Leptonia from "./icons/services/ml/leptonia.svg";
+import MetaAi from "./icons/services/ml/metaai.svg";
+import Microsoft from "./icons/services/ml/microsoft.svg";
+import OpenAI from "./icons/services/ml/openai.svg";
+
 export const Assets = {
   Icons: {
     AppIcon: AppIcon,
-    Home:{
-        Icon1:icon1,
-        Icon2:icon2,
-        Icon3:icon3,
-        Icon4:icon4,
-        Icon5:icon5,
-        Icon6:icon6,
-        Icon7:icon7,
-        Icon8:icon8,
-        Icon9:icon9,
-        Icon10:icon10,
-        Icon11:icon11,
-        Icon12:icon12,
-        Icon13:icon13,
-        Icon14:icon14
-
-    }
+    Home: {
+      Icon1: icon1,
+      Icon2: icon2,
+      Icon3: icon3,
+      Icon4: icon4,
+      Icon5: icon5,
+      Icon6: icon6,
+      Icon7: icon7,
+      Icon8: icon8,
+      Icon9: icon9,
+      Icon10: icon10,
+      Icon11: icon11,
+      Icon12: icon12,
+      Icon13: icon13,
+      Icon14: icon14,
+    },
+    Services: {
+      ML: {
+        HaggingFace: HaggingFaceIcon,
+        Apache: Apache,
+        AWS: AWS,
+        Leptonia: Leptonia,
+        MetaAi: MetaAi,
+        Microsoft: Microsoft,
+        OpenAI: OpenAI,
+      },
+    },
   },
   Images: {
     Home: {
       BG1: BG1,
       Techno: Tech,
-      ML:ML,
-      BuyNow:BuyNow,
-      CD:CreativeDes,
-      CreativeChat:CreativeChat,
-      Agriculture:Agriculture
+      ML: ML,
+      BuyNow: BuyNow,
+      CD: CreativeDes,
+      CreativeChat: CreativeChat,
+      Agriculture: Agriculture,
+    },
+    Services: {
+      ML: {
+        AiImage: AiImage,
+        WomenImage: WomenImage,
+      },
     },
   },
 };
